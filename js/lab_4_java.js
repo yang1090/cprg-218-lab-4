5d8c823e6fbd65d71d178d785cd5850dd09ea8d0
yang1090 <samyeung1090@gmail.com>
2024-03-21 22:46:34 -0600

    re-submission

--- js/lab_4_java.js
+++ js/lab_4_java.js
@@ -1,16 +1,15 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+const acc = document.querySelectorAll(".accordion");
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+acc.forEach(item => {
+  item.addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxHeight = `${panel.scrollHeight}px`;
     } 
   });
-}
+});
 
 document.getElementById('year').innerHTML = new Date().getFullYear();
